19854b111ba931c488e71ed303d57a4ef3a91696
r0452105 <61143931+r0452105@users.noreply.github.com>
2021-05-03 11:38:13 +0200

    startingOVEST

---
+++ assets/MAPPA FOLIGNO RPG OVEST/AIUOLA.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="AIUOLA" tilewidth="48" tileheight="48" tilecount="12" columns="4">
+ <image source="TILES/tilesets/AIUOLA.png" width="192" height="144"/>
+</tileset>
---
+++ assets/MAPPA FOLIGNO RPG OVEST/CHIESA.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="CHIESA" tilewidth="48" tileheight="48" tilecount="72" columns="12">
+ <image source="TILES/oaz5u3y9.png" width="576" height="288"/>
+</tileset>
